1fa582fe261e1fd1666e4e2cbc14a982e8f15700
unknown <abdulrehman03365@gmail.com>
2024-10-03 11:41:31 +0500

    Initialize Framework and smoke test suite

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,28 @@
 const { defineConfig } = require("cypress");
+ const dotenv = require('dotenv');
+// import cypress from "cypress";
+// Load environment variables from the .env file
+ dotenv.config();
+
+// Access the baseUrl from process.env (loaded via dotenv)
+const baseUrl = process.env.BASE_URL;
+
+// Debugging: Log the base URL
+console.log("Base URL:", baseUrl);
 
 module.exports = defineConfig({
+  experimentalSessionAndOrigin: true,
   e2e: {
+    baseUrl: baseUrl,  // Use process.env.baseUrl here
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,    // Generates an HTML report
+      json: false,   // Generates a JSON report
+    },
+    screenshotOnRunFailure: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/smoke-tests/smoketests.cy.js
@@ -0,0 +1,64 @@
+import 'cypress-file-upload';
+import { it } from 'mocha';
+require('cypress-xpath');
+
+describe('SQA Demo tests', () => {
+
+  before(() => {
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      // Log the error for debugging, but don't fail the test
+      console.error(err);
+      return false; // prevents Cypress from failing the test
+    });
+    cy.visit('/');
+    cy.get('.btn-signin').click();
+    cy.xpath("//div[@class='email-address-main-div']//input[@type='email']").type('abdulrehman03365@gmail.com');
+    cy.get('input[type="password"]').should('exist').type('Password#1');
+    cy.get('.signin-first-div').click() 
+
+
+  });
+
+  // beforeEach(() => {
+  //   // const baseUrl=Cypress.env('baseUrl')
+  //   cy.visit('/');
+   
+  // });
+
+  // it('Login Test',()=>{
+ 
+  //   cy.get('.btn-signin').click();
+  //   cy.xpath("//div[@class='email-address-main-div']//input[@type='email']").type('abdulrehman03365@gmail.com');
+  //   cy.get('input[type="password"]').should('exist').type('Password#1');
+  //   cy.get('.signin-first-div').click()
+  // })
+
+  // it('Game UI Test',()=>{
+  //   // cy.fixture('example').then((data)=>{
+  //   // cy.visit(data.contestUrl)
+  //   // cy.get('.enter-btn-div').click()
+  //   // cy.contains('$10').click({ force: true });
+   
+
+  //   })
+  // })
+
+  
+
+  it('Demo test broken links', () => {
+    cy.get('a').each($el => {
+      const href = $el.prop('href'); 
+      if (href && !href.includes('x.com')) { 
+        cy.request(href).then((response) => {
+          expect(response.status).to.eq(200); 
+        });
+      } else {
+        cy.log('Skipped URL:', href);
+      }
+    });
+  });
+
+  it("Demo  entry test", ()=>{
+    
+  })
+});
